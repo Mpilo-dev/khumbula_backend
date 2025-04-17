e70a6401bc2b09793e616671ea92f3a508f2ecce
Mpilo-dev <ndlovumpilo@icloud.com>
2025-04-17 14:40:07 +0200

    Add packages and Update app.js and server.js

--- server.js
+++ server.js
@@ -2,22 +2,39 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
+const scheduleUserAlerts = require("./services/alertScheduler");
+
+process.on("uncaughtException", (err) => {
+  console.log("uncaughtException REJECTION! Shutting down...");
+  console.log(`ERROR_NAME: ${err.name}, ERROR_MESSAGE: ${err.message}`);
+  process.exit(1);
+});
+
 const app = require("./app");
 
-// Database connection
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
-mongoose
-  .connect(DB)
-  .then(() => console.log("DB Connection SUCCESSFUL!"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+
+mongoose.connect(DB).then(() => {
+  console.log("DB Connection SUCCESSFUL!");
+
+  scheduleUserAlerts();
+});
 
 mongoose.set("strictQuery", false);
 
-// start server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Async rejections and how do we start up the server/app again
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(`ERROR_NAME: ${err.name}, ERROR_MESSAGE: ${err.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
